73dd9b1a8c89d9149b23fec126d2e31cc01eddad
NipuniWickramasinghe <81399550+NipuniWickramasinghe@users.noreply.github.com>
2024-02-15 13:43:31 +0000

    Change Build.sh file

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
-docker build -f Dockerfile --tag bistecglobal/pghbricks-agent:1.0.0 .
+# docker build -f Dockerfile --tag bistecglobal/pghbricks-agent:1.0.0 .
+docker build -f studio/Dockerfile_studio --tag bistecglobal/recrutement-agent:1.0.0 .
